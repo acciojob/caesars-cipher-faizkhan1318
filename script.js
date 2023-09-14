9c120e70404d8090eae716cd4d6301e74f671eaa
Faisel Khan <125124484+faizkhan1318@users.noreply.github.com>
2023-09-14 14:35:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,14 +35,23 @@ function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
 	for(let i=0; i<encodedStr.length; i++){
-		decodedArr.push(lookup[encodedStr[i]]);
-	}
-  return decodedArr; //return decodedArr
+        let char = encodedStr[i];
+        if(lookup[char] !== undefined){
+			//search in the obj list and push the charater
+            decodedArr.push(lookup[char]);
+        }else{
+			//if no such char is present then push the charater unchanged
+            decodedArr.push(char);
+        }
+          
+      }
+      
+    return decodedArr.join(''); //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 // Do not change this line
 window.rot13 = rot13;
